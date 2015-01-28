c462e1a3c701878215e8224acd8e3e3e0e2182f8
Amy Winarske <amy.winarske@magnet.com>
2015-01-28 12:04:43 -0800

    Adjust pathnames for MMX smack

--- patch/4.0/dnsjava.sh
+++ patch/4.0/dnsjava.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo $PWD
-cp -r ../../src/smack/smack-resolver-dnsjava/src/main/java/ .
+cp -r ../../src/smack/mmx-smack-resolver-dnsjava/src/main/java/ .
 rm -rf org/jivesoftware/smack/util/dns/minidns
